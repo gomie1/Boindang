17f1bbeb6f41e3b8447bb05e99a12073d7e503e0
ì Nageum Jeong <nagum0545@naver.com>
2025-05-21 13:34:34 +0900

    feat: 댓글 작성 api 추가 구현

--- backend/community/src/main/java/com/boindang/community/dto/request/CreateCommentRequest.java
+++ backend/community/src/main/java/com/boindang/community/dto/request/CreateCommentRequest.java
@@ -1,4 +1,16 @@
 package com.boindang.community.dto.request;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class CreateCommentRequest {
+	private String content;
+
+	@Builder
+	public CreateCommentRequest(String content) {
+		this.content = content;
+	}
 }
