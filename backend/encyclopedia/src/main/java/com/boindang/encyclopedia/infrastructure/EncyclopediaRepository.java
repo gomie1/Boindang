4919007d0a058b8cfd58ce4680a0490ef115edc7
ì Nageum Jeong <nagum0545@naver.com>
2025-05-21 17:37:32 +0900

    fix: 리포트 성분 검색 다시 수정

--- backend/encyclopedia/src/main/java/com/boindang/encyclopedia/infrastructure/EncyclopediaRepository.java
+++ backend/encyclopedia/src/main/java/com/boindang/encyclopedia/infrastructure/EncyclopediaRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Repository
 public interface EncyclopediaRepository extends ElasticsearchRepository<IngredientDictionary, String> {
     List<IngredientDictionary> findByNameContaining(String query);
+
+    @Query("{ \"terms\": { \"name.keyword\": ?0 } }")
     List<IngredientDictionary> findByNameIn(List<String> names);
     List<IngredientDictionary> findByIdIn(List<String> ids);
     List<IngredientDictionary> findByName(String name);
